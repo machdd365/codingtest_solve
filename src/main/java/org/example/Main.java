3b6eed1b79ec6dfe43960957885a29f3e18c5752
machdd <jkk1637@naver.com>
2022-12-31 10:21:00 +0900

    no message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -57,6 +57,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
 
 

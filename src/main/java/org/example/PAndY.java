4bde8439b479c22175d2b7c691e0aeb49234b311
machdd <jkk1637@naver.com>
2023-04-04 10:21:00 +0900

    ddd

---
+++ src/main/java/org/example/PAndY.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class PAndY {
+    boolean solution(String s) {
+        s = s.toUpperCase();
+
+        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+    }
+}

088bab5232a08117fc0355047dd1341f5ea14012
machdd <jkk1637@naver.com>
2023-05-05 22:30:25 +0900

    no message

---
+++ src/main/java/org/example/threejinbub.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+
+        while(n != 0) {
+            if(n%3 == 0) {
+                list.add(0);
+            } else if(n%3 == 1) {
+                list.add(1);
+            } else if(n%3 == 2) {
+                list.add(2);
+            }
+            n /= 3;
+        }
+
+        int x = 1;
+        for(int i=list.size()-1; i>=0; i--) {
+            answer += list.get(i)*x;
+            x *= 3;
+        }
+
+        return answer;
+    }
+}

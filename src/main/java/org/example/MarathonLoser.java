41e2273f7957f24487eca5f5475273348bc534af
machdd <jkk1637@naver.com>
2023-08-17 22:56:45 +0900

    no message

--- src/main/java/org/example/MarathonLoser.java
+++ src/main/java/org/example/MarathonLoser.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class MarathonLoser {
     public static void main(String[] args){
         String[] participant = {"mislav", "stanko", "mislav", "ana"};
@@ -8,22 +10,35 @@ public class MarathonLoser {
         System.out.println(marathonLoser(participant, completion));
     }
 
+//    public static String marathonLoser(String[] participant,String[] completion){
+//        int i = 0;
+//
+//        for (int j=0; j< completion.length; j++){
+//            if(participant[j].equals(completion[i])){
+//                System.out.println(participant[j]);
+//                participant[j] = "0";
+//                i++;
+//            }
+//        }
+//        System.out.println(Arrays.toString(participant));
+//
+//        for (int j=0; j< participant.length; j++){
+//            if (!(participant[j].equals("0"))){
+//                return participant[j];
+//            }
+//        }
+//
+//        return "그럴리가 없어";
+//    }
+
     public static String marathonLoser(String[] participant,String[] completion){
-        int i = 0;
-
-        for (int j=0; j< completion.length; j++){
-            if(participant[i].equals(completion[j])){
-                completion[j] = "0";
-                i++;
+        int j=0;
+        for (int i : completion){
+            if (participant[j].equals(i)) {
+                participant[j] = "0";
+            }else {
+                j++;
             }
         }
-
-        for (int j=0; j< participant.length; j++){
-            if (!(participant[j].equals("0"))){
-                return participant[j];
-            }
-        }
-
-        return "그럴리가 없어";
     }
 }

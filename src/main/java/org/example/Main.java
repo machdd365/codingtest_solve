606adcdb8cf70faec10e331aaabb7bee4dc96ddd
machdd <jkk1637@naver.com>
2024-01-26 10:51:00 +0900

    ddd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -151,5 +151,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
